fd17e666adc717c55299ab26e304f3664dbace9f
Víctor Macêdo <129907432+nomevict@users.noreply.github.com>
2023-06-27 22:57:55 -0300

    Delete login.py

--- login.py
+++
@@ -1,9 +0,0 @@
-class Login:
-    def __init__(self, id_usuario, username, senha):
-        self.id_usuario = id_usuario
-        self.username = username
-        self.senha = senha
-
-
-
-        
